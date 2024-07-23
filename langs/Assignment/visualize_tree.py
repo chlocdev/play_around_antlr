ea93ae6d89a479fd1f784cfb3a6c8fcd2b6c02bc
locchuong <locchuong123@gmail.com>
2024-07-24 06:20:12 +0700

    update

--- langs/Assignment/visualize_tree.py
+++ langs/Assignment/visualize_tree.py
@@ -45,9 +45,11 @@ def traverse_tree(dot, tree):
     if tree.getChildCount() > 0:
         # Get parent string
         parent_str = get_node_name(tree)
+
         for child in tree.children:
             # Get child string
             child_str = get_node_name(child)
+
             # Give a edge from parent - child
             dot.edge(parent_str, child_str)
             # Continue traverse to recursively
